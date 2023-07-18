d37f384680c757671de9c2455880216a6f8bface
Rajender Singh <75353041+rajender8166@users.noreply.github.com>
2023-07-18 18:52:35 +0530

    SmartContractExample.sol

--- SmartContractExample.sol
+++ SmartContractExample.sol
@@ -5,20 +5,20 @@ contract Errorhanding{
 
     uint x=2;
 
-    function test(uint c) public pure {
+    function merci(uint c) public pure {
         assert(c!=0);
         // No assert() statement since we use require() instead.
-        // The function will check if 'num' is not zero. If it is zero, it will revert with the specified error message.
+        // The function will check if 'num' is not zero. If it is zero, it will revert to the specified error message.
     }
 
-    function divide(uint _numerator,uint _denomenator) public pure returns(uint){
+    function raju(uint _numerator,uint _denomenator) public pure returns(uint){
         if(_numerator<_denomenator){
             revert("Enter the number greater than denomentor");
         }
         return _numerator/_denomenator;
     }
     
-    function mult(uint y) public view returns(uint){
+    function rajender(uint y) public view returns(uint){
         require(y>0,"Enter the Number greater than Zero");
         return y*x;
     }
